d4fdc3c1071591d07cee3e54691965dcf35a9c6f
Luciano <Luciano@localhost>
2019-02-28 22:15:45 -0300

    Finalização aula 03

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/dao/LivroDao.java
@@ -0,0 +1,25 @@
+package br.com.casadocodigo.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.casadocodigo.models.Livro;
+
+public class LivroDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void salvar(Livro livro) {
+		manager.persist(livro);
+	}
+
+	public List<Livro> listar() {
+		String jpql = "select distinct(l) from Livro l"
+	            + " join fetch l.autores";
+
+		return manager.createQuery(jpql, Livro.class).getResultList();
+	}
+}
